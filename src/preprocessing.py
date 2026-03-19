c05e125ec60a28a01a547d5bca4405827839ce29
c0linhu1 <141664358+c0linhu1@users.noreply.github.com>
2026-03-18 22:08:05 -0400

    libs

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,17 @@
 """
 preprocessing.py
 
+raw test contains html tags, weird unicode, incosistensies, whitespace ,etc
+first step is always cleaning/standardizing text into a clean format that 
+every downstream component can rely on.
+
 """
+
+import re
+import html
+import unicodedata
+from pathlib import Path
+
+import pandas as pd
+import spacy
+from datasets import load_dataset
